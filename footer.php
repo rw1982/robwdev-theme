78831860dbe29980404883ae7524e2c23ccef681
robw1982 <williams.1982@gmail.com>
2016-09-18 01:23:59 -0400

    added bases theme files

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package robwdev-theme
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'robwdev-theme' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'robwdev-theme' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'robwdev-theme' ), 'robwdev-theme', '<a href="http://robwdev.com" rel="designer">robw</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
